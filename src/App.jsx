292ddd8740774d8c97262815ab6ceb96f16347bf
wasupalonely <juandiegomaciasoy@gmail.com>
2022-10-03 21:16:11 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Wrapper from './components/Wrappers'
+import Title from './components/Title'
+
+function App() {
+  return (
+    <Wrapper>
+      <Title>
+        Hello World!
+      </Title>
+    </Wrapper>
+  )
+}
+
+export default App
